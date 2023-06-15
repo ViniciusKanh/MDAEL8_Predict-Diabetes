7d7a5e3d1905a893aa9dcdc594219b4747976142
Vinicius Santos <vinnyciussouza@outlook.com>
2023-06-15 00:51:44 -0300

    up
    up

--- 1-Preprocessing/Analise Descritiva - Medidas/AnaliseDescritivaMedida.py
+++ 1-Preprocessing/Analise Descritiva - Medidas/AnaliseDescritivaMedida.py
@@ -6,7 +6,7 @@ def main():
     # Faz a leitura do arquivo
     input_file = '0-Datasets/diabetesClear.data'
     names = ['Número Gestações','Glucose','pressao Arterial','Expessura da Pele','Insulina','IMC','Função Pedigree Diabete','Idade','Resultado'] 
-    target = 'IMC'
+    target = 'Glucose'
     df = pd.read_csv(input_file,    # Nome do arquivo com dados
                      names = names) # Nome das colunas
     
